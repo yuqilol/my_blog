9251a8da5e25a8adf685b07581765b0b46ea8773
yuqi <819925806@qq.com>
2021-12-08 18:04:05 +0800

    my-blog

---
+++ my-blog/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <add-blog></add-blog>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld'
+import AddBlog from './components/addblog'
+
+export default {
+  name: 'App',
+  components: {
+    AddBlog
+  }
+}
+</script>
+
+<style>
+
+</style>
